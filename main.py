365122103d020214da7972bd0013a83c0236222f
Robin Fleige <lq250@stud.uni-heidelberg.de>
2022-07-04 14:17:50 +0200

    Several changes

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import numpy as np
 import sys
-import math
 from jax import grad, jacfwd
+from sympy import *
+from scipy.optimize import fsolve
+import vtkmodules.vtkInteractionStyle
+import vtkmodules.vtkRenderingOpenGL2
+from vtkmodules.vtkCommonColor import vtkNamedColors
 from vtkmodules.vtkInteractionStyle import vtkInteractorStyleTrackballCamera
 from vtkmodules.vtkCommonCore import vtkPoints, vtkUnsignedCharArray
 from vtkmodules.vtkCommonDataModel import (
     vtkCellArray,
-    vtkPolyData
+    vtkPolyData,
+    vtkPolyLine
 )
 from vtkmodules.vtkRenderingCore import (
     vtkActor,
@@ -24,11 +30,6 @@ def f_2p2d_simple_y(vars):
     s, t, x, y = vars
     return y-s
 
-def f_2p2d_simple_z(vars):
-    s, t, x, y = vars
-    return 2*x
-
-
 
 f_2p2d_simple = [f_2p2d_simple_x, f_2p2d_simple_y]
 
@@ -44,19 +45,6 @@ def f_2p2d_double_y(vars):
 
 
 f_2p2d_double = [f_2p2d_double_x, f_2p2d_double_y]
-
-
-def f_2p2d_circle_x(vars):
-    s, t, x, y = vars
-    return x**2+s**2+t**2-1
-
-
-def f_2p2d_circle_y(vars):
-    s, t, x, y = vars
-    return y
-
-
-f_2p2d_circle = [f_2p2d_circle_x, f_2p2d_circle_y]
 
 
 def f_3p3d_circle_x(vars):
@@ -77,10 +65,33 @@ def f_3p3d_circle_z(vars):
 f_3p3d_circle = [f_3p3d_circle_x, f_3p3d_circle_y, f_3p3d_circle_z]
 
 
+def f_2p2d_circle_x(vars):
+    s, t, x, y = vars
+    return x**2+s**2+t**2-1
+
+
+def f_2p2d_circle_y(vars):
+    s, t, x, y = vars
+    return y
+
+
+def f_2p2d_circle_z(vars):
+    s, t, x, y = vars
+    return 2*x
+
+
+f_2p2d_circle = [f_2p2d_circle_x, f_2p2d_circle_y]
+
+class CritLine:
+    def __init__(self):
+        self.lower_order_critlines = []
+        self.bifurcation_point = []
+        self.crit_list = []
+
 class VectorField:
-    DETAIL = 3
-    DELTA = 1/DETAIL
-    VECTOR_SIZE_THRESHOLD = 0.000000000001
+    DETAIL = 10
+    DELTA = 2/DETAIL
+    VECTOR_SIZE_THRESHOLD = 0.0001
     MIN = -4
     MAX = 4
 
@@ -129,7 +140,7 @@ class VectorField:
                 for y in range(self.space_dimensions):
                     line.append(float(values[ids[fff_component + y]]))
                 matrix.append(line)
-            fff.append(pow(-1,(fff_component+1)*self.space_dimensions)*self.determinant(matrix))
+            fff.append(pow(-1, (fff_component+1)*self.space_dimensions)*self.determinant(matrix))
         return fff
 
     # Only the Parameter Part
@@ -166,7 +177,7 @@ class VectorField:
         value = 0
         for i in range(self.space_dimensions):
             value += vector[i]**2
-        return math.sqrt(value)
+        return sqrt(value)
 
     def newton_step(self, point):
         both = []
@@ -179,12 +190,11 @@ class VectorField:
             maxs.append(point[i+self.parameter_dimensions]+self.DELTA)
         return both, mins, maxs
 
-    def critical_points(self, both, mins, maxs, recursion_depth, min_recursion_depth):
+    def critical_points(self, both, mins, maxs, recursion_depth, min_recursion):
         center = both + [(mins[i]+maxs[i])/2 for i in range(self.space_dimensions)]
-        if (self.vector_length([self.base_function[i](center) for i in range(self.space_dimensions)]) < self.VECTOR_SIZE_THRESHOLD and min_recursion_depth <= 0) or recursion_depth == sys.getrecursionlimit()-20:
+        if self.vector_length([self.base_function[i](center) for i in range(self.space_dimensions)]) < self.VECTOR_SIZE_THRESHOLD or recursion_depth == sys.getrecursionlimit()-20:
             return [center]
         else:
-
             corners = []
             for i in range(pow(2, self.space_dimensions)):
                 corner = []
@@ -201,9 +211,9 @@ class VectorField:
                 positive_counts.append(0)
 
             for corner in corners:
-                value = [self.base_function[i](corner) for i in range(self.space_dimensions)]
                 for i in range(self.space_dimensions):
-                    if value[i] > 0:
+                    value = self.base_function[i](corner)
+                    if value > 0:
                         positive_counts[i] = positive_counts[i] + 1
 
             contains_crit = True
@@ -211,7 +221,7 @@ class VectorField:
                 if positive_counts[i] == 0 or positive_counts[i] == len(corners):
                     contains_crit = False
 
-            if contains_crit or min_recursion_depth > 0:
+            if contains_crit or min_recursion > 0:
                 new_mins = []
                 new_maxs = []
                 for i in range(pow(2, self.space_dimensions)):
@@ -229,7 +239,7 @@ class VectorField:
 
                 crits = []
                 for i in range(len(new_mins)):
-                    crits = crits + self.critical_points(both, new_mins[i], new_maxs[i], recursion_depth + 1, min_recursion_depth - 1)
+                    crits = crits + self.critical_points(both, new_mins[i], new_maxs[i], recursion_depth + 1, min_recursion - 1)
                 return crits
             else:
                 return []
@@ -255,15 +265,18 @@ class VectorField:
                 next_point[j + self.parameter_dimensions] + (fff[j] / self.DETAIL) * pow(-1, direction))
 
         next_both, next_mins, next_maxs = self.newton_step(next_point)
-        next_crit = self.critical_points(next_both, next_mins, next_maxs, 0, 0)
+        next_crit = self.critical_points(next_both, next_mins, next_maxs, 0, 2)
 
         return next_crit
 
     def crit_line(self, start, start_at_bif):
-        crits = []
         nexts = []
+        critline = CritLine()
         if not start_at_bif:
-            crits.append(start)
+            critline.crit_list.append(start)
+        else:
+            critline.bifurcation_point = start
+
 
         for i in range(2):
             temp = self.fff_step(start, i)
@@ -272,9 +285,12 @@ class VectorField:
 
         while len(nexts) > 0:
             current = nexts[0]
+            #print(current)
             nexts.remove(current)
-            crits.append(current[0])
-
+            if current[1] == 0:
+                critline.crit_list.append(current[0])
+            else:
+                critline.crit_list.insert(0, current[0])
             in_range = True
             for i in range(self.space_dimensions+1):
                 if current[0][i] < self.MIN or current[0][i] > self.MAX:
@@ -285,78 +301,152 @@ class VectorField:
                 if temp:
                     nexts.append([temp[0], current[1]])
 
-        return crits
+        return critline
 
 
-def render(crits, bifurcations):
-    points = vtkPoints()
-    vertices = vtkCellArray()
-    polydata = vtkPolyData()
-    colors = vtkUnsignedCharArray()
-    colors.SetNumberOfComponents(4)
-    colors.SetNumberOfTuples(len(crits) + len(bifurcations))
-
-    for i in range(len(crits)):
-        rgba = [255, 0, 0, 255]
-        colors.InsertTuple(i, rgba)
-        p = [crits[i][0], crits[i][1], crits[i][2]]
-        pid = points.InsertNextPoint(p)
-        vertices.InsertNextCell(1)
-        vertices.InsertCellPoint(pid)
-
-    for i in range(len(bifurcations)):
-        rgba = [0, 255, 0, 255]
-        colors.InsertTuple(i + len(crits), rgba)
-        p = [bifurcations[i][0], bifurcations[i][1], bifurcations[i][2]]
-        pid = points.InsertNextPoint(p)
-        vertices.InsertNextCell(1)
-        vertices.InsertCellPoint(pid)
-
-    polydata.SetPoints(points)
-    polydata.SetVerts(vertices)
-    polydata.GetCellData().SetScalars(colors);
-    mapper = vtkPolyDataMapper()
-    mapper.SetInputData(polydata)
-    actor = vtkActor()
-    actor.SetMapper(mapper)
-    actor.GetProperty().SetPointSize(10)
-    renderer = vtkRenderer()
-    renderer.AddActor(actor)
-    renderWindow = vtkRenderWindow()
-    renderWindow.AddRenderer(renderer)
-    renderer.SetBackground([0, 0, 255])
-    interactor = vtkRenderWindowInteractor()
-    interactor.SetRenderWindow(renderWindow)
-    style = vtkInteractorStyleTrackballCamera()
-    interactor.SetInteractorStyle(style)
-    renderWindow.Render()
-    interactor.Start()
-
-#Start somewhere to extract highest bifurcation
-#vf = VectorField(f_2p2d_simple, 2, 2)
-#m_vf = VectorField([vf.fff_cond, f_2p2d_simple_x, f_2p2d_simple_y], 1, 3)
-
-#start = vf.critical_points([1,1],[vf.MIN,vf.MIN],[vf.MAX,vf.MAX],0,5)
-#print(start)
-
-#Render Bifurcations and Crits from Bifurcation
 vf = VectorField(f_2p2d_circle, 2, 2)
-m_vf = VectorField([vf.fff_cond, f_2p2d_circle_x, f_2p2d_circle_y], 1, 3)
+m_vf = VectorField([f_2p2d_circle_z, f_2p2d_circle_x, f_2p2d_circle_y], 1, 3)
 
-bifurcations = m_vf.crit_line([1.0, 0.0, 0.0, 0.0], False)
-crits = []
+bif_line = m_vf.crit_line([0.0, 1.0, 0.0, 0.0], True)
+bifurcations = bif_line.crit_list.copy()
+
 print(len(bifurcations))
 for i in range(len(bifurcations)):
     print(i)
-    crits = crits + vf.crit_line(bifurcations[i], True)
+    crit_line = vf.crit_line(bifurcations[i], True)
+    bif_line.lower_order_critlines.append(crit_line)
 
-#render(crits, bifurcations)
+points = vtkPoints()
+vertices = vtkCellArray()
+polydata = vtkPolyData()
+cells = vtkCellArray()
+colors = vtkUnsignedCharArray()
+
+colors.SetNumberOfComponents(4)
+colors.SetNumberOfTuples(len(bif_line.lower_order_critlines)+1)
+id = 0
+
+rgba = [0, 255, 0, 255]
+colors.InsertTuple(0, rgba)
+bif_poly_line = vtkPolyLine()
+bif_poly_line.GetPointIds().SetNumberOfIds(len(bifurcations))
+for i in range(len(bif_line.crit_list)):
+    points.InsertNextPoint([bif_line.crit_list[i][0], bif_line.crit_list[i][1], bif_line.crit_list[i][2]])
+    bif_poly_line.GetPointIds().SetId(i, id)
+    id = id + 1
+cells.InsertNextCell(bif_poly_line)
+for i in range(len(bif_line.lower_order_critlines)):
+    rgba = [255, 0, 0, 255]
+    colors.InsertTuple(i+1, rgba)
+    crit_poly_line = vtkPolyLine()
+    crit_poly_line.GetPointIds().SetNumberOfIds(len(bif_line.lower_order_critlines[i].crit_list))
+    for j in range(len(bif_line.lower_order_critlines[i].crit_list)):
+        points.InsertNextPoint([bif_line.lower_order_critlines[i].crit_list[j][0], bif_line.lower_order_critlines[i].crit_list[j][1], bif_line.lower_order_critlines[i].crit_list[j][2]])
+        crit_poly_line.GetPointIds().SetId(j, id)
+        id = id + 1
+    cells.InsertNextCell(crit_poly_line)
+
+polydata.SetLines(cells)
+polydata.SetPoints(points)
+polydata.GetCellData().SetScalars(colors);
+
+
+#    colors = vtkUnsignedCharArray()
+#    colors.SetNumberOfComponents(4)
+#    colors.SetNumberOfTuples(len(crits)+len(bifurcations))
+#
+#    for i in range(len(crits)):
+#        rgba = [255, 0, 0, 255]
+#        colors.InsertTuple(i, rgba)
+#        p = [crits[i][0], crits[i][1], crits[i][2]]
+#        pid = points.InsertNextPoint(p)
+#        vertices.InsertNextCell(1)
+#        vertices.InsertCellPoint(pid)
+#
+#    for i in range(len(bifurcations)):
+#        rgba = [0, 255, 0, 255]
+#        colors.InsertTuple(i+len(crits), rgba)
+#        p = [bifurcations[i][0], bifurcations[i][1], bifurcations[i][2]]
+#        pid = points.InsertNextPoint(p)
+#        vertices.InsertNextCell(1)
+#        vertices.InsertCellPoint(pid)
+#
+#
+#    polydata.SetPoints(points)
+#    polydata.SetVerts(vertices)
+#    polydata.GetCellData().SetScalars(colors);
 
 
 
+mapper = vtkPolyDataMapper()
+mapper.SetInputData(polydata)
+actor = vtkActor()
+actor.SetMapper(mapper)
+actor.GetProperty().SetPointSize(10)
+renderer = vtkRenderer()
+renderer.AddActor(actor)
+renderWindow = vtkRenderWindow()
+renderWindow.AddRenderer(renderer)
+renderer.SetBackground([0, 0, 255])
+interactor = vtkRenderWindowInteractor()
+interactor.SetRenderWindow(renderWindow)
+style = vtkInteractorStyleTrackballCamera()
+interactor.SetInteractorStyle(style)
+renderWindow.Render()
+interactor.Start()
+
+#8 Seiten + Refferenzen
+#Sehr weit vorne anfangen; Kritische Punkte, Parameterabhängige Vektorfelder, FFF, Bifurkationen; Man muss alles durch das Paper verstehen; Stromlinie = Integralkurve des stationären Feldes;
+#Introduction
+#Related Works
+#Fundamentals
+#Method
+#Konzept
+#Diskretisierung
+#1/4 Seite Implementierung
+#Results
+
+#Feld Extraktion nochmal genauer angucken
+
+#Parametrisierung aufschreiben (Länge entlang der einen Richtung, Länge entlang der zweiten Kurve)
+
+#Newton Iteration nochmal neu probieren
+#Stable Feature Flow Field
+#Philipp entlang hoher Mannigfaltigkeiten mit Wedge Product
+
+#Höhere Bifurcation kann durch Vorzeichenwechsel erkannt werden, wenn die Schrittweise über 1 ist
+
+#Bei welchen Ids fehlen ide Linien: An die Stellen Debuggen
+
+#Folien beschreiben was ich gemacht habe
+#Work Packages
+
+#mark.ladd@dkfz-heidelberg.de
+#dachsbacher@kit.edu
+# TAC Meeting: Mail an Mark Ladd (DKFZ) schreiben; Karsten Dachsbacher (HIDSS4Health TAC) einladen; Doodle aufsetzen; 25.7-29.7; Wenn es nicht geht, alternativen anbieten
+
+# TODO Promotionsunterlagen
+# TODO Phd Student Gitlab Project anlegen
+# TODO Paper Gitlab Project anlegen
+# TODO HGS anmelden
+
+# TODO Move to Bifurcation from Start
+# TODO Template runterladen
+# TODO Rand Bifurcationen berechnen
+# TODO Flächen rendern
+# TODO Fehlende Linien fixen
+# TODO Merge CritLines
+
+#TAC Meeting: 2-3 Wochen; Mark Ladd, Karsten Dachsbacher (KIT); Filip Sadlo
+#Orthogonal: Dependent Vectors Operator; Wedge Product
+#Dependent Vectors Flow Field
+#Periodische Orbits finden? Bifurcation Gegenstück dazu? Mehr als zwei Parameter; Boundary Switch Curves? (Theisel, Weinkauf)(Bifurkationsstrukturen davon); Analyse schneller als Generierung der Daten; Großteil der hochdimensionalen Daten vermutlich analytisch
 
 
-#FFF korrekt(er):           http://vc.cs.ovgu.de/index.php?article_id=3&clang=0&bibtex_key=Theisel_2003_VisSym
+#Erhöhtes Delta + Min Recursion bei Step = volles Kreis, aber nur halbe Kugel
+# Wenn Kreis voll, sucht er von vorne und hinten die critline, was zu gedoppelten Punkten führt
+
 #FFF Initial:               http://vc.cs.ovgu.de/files/publications/Archive/Theisel_2003_VisSym.pdf
-#FFF Allgemein:             Sign nur bei ungerader Anzahl an Dimensionen (entweder über Determinante oder Gram-Schmidts)
+#FFF korrekt(er):           http://vc.cs.ovgu.de/index.php?article_id=3&clang=0&bibtex_key=Theisel_2003_VisSym
 #Calculate Orthogonal:      https://www.geometrictools.com/Documentation/OrthonormalSets.pdf
+#FFF Allgemein:             Sign nur bei ungerader Anzahl an Dimensionen (entweder über Determinante oder Gram-Schmidts)
